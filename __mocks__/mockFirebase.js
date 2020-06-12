e70f86a9d0e6f8c4f32dea9a7324475417f89451
ant4u <anthonyimontalto@gmail.com>
2020-06-12 00:17:24 -0400

    fixed test

--- __mocks__/mockFirebase.js
+++ __mocks__/mockFirebase.js
@@ -1,17 +1,8 @@
 /* eslint-disable no-undef */
-jest.mock('react-native-firebase', () => ({
-    messaging: jest.fn(() => ({
-      hasPermission: jest.fn(() => Promise.resolve(true)),
-      subscribeToTopic: jest.fn(),
-      unsubscribeFromTopic: jest.fn(),
-      requestPermission: jest.fn(() => Promise.resolve(true)),
-      getToken: jest.fn(() => Promise.resolve('myMockToken')),
-    })),
-    notifications: jest.fn(() => ({
-      onNotification: jest.fn(),
-      onNotificationDisplayed: jest.fn(),
-    })),
-    analytics: jest.fn(() => ({
-      logEvent: jest.fn(),
-    })),
-  }));
+//import auth from '@react-native-firebase/auth';
+
+jest.mock('@react-native-firebase/auth', () => {
+  return () => ({
+    auth: jest.fn(),
+  });
+});
