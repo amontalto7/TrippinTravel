3177649e663be04ca4a4f0bbd95c55f46f6d3ac3
ant4u <anthonyimontalto@gmail.com>
2020-09-29 15:33:11 -0400

    fixed buttons

--- src/App.tsx
+++ src/App.tsx
@@ -9,45 +9,32 @@ import 'react-native-gesture-handler';
  * @format
  */
 import * as React from 'react';
-import {NavigationContainer, DrawerActions, getFocusedRouteNameFromRoute} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DrawerActions,
+  getFocusedRouteNameFromRoute,
+} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createStackNavigator} from '@react-navigation/stack';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  StatusBar,
-} from 'react-native';
 
-import {
-  Container,
-  Header,
-  Title,
-  Left,
-  Right,
-  Button,
-  Body,
-  Content,
-  Text,
-  Card,
-  CardItem,
-} from 'native-base';
+import { Button } from 'react-native';
 
-import LandingScreen from "./screens/Landing";
-import Profile from "./screens/Profile";
-import PasswordForgetScreen from "./screens/PasswordForget";
-import PasswordChangeScreen from "./screens/PasswordChange";
-import SignInScreen from "./screens/SignIn";
-import SignUpScreen from "./screens/SignUp";
-import HomeScreen from "./screens/Home";
+import LandingScreen from './screens/Landing';
+import Profile from './screens/Profile';
+import PasswordForgetScreen from './screens/PasswordForget';
+import PasswordChangeScreen from './screens/PasswordChange';
+import SignInScreen from './screens/SignIn';
+import SignUpScreen from './screens/SignUp';
+import HomeScreen from './screens/Home';
+import AccountScreen from './screens/Account';
 
 import auth from '@react-native-firebase/auth';
+declare const global: {HermesInternal: null | {}};
 
 
 const Tab = createBottomTabNavigator();
- 
+
 const HomeTabs = () => {
   return (
     <Tab.Navigator>
@@ -57,7 +44,6 @@ const HomeTabs = () => {
   );
 };
 
-declare const global: {HermesInternal: null | {}};
 
 const Drawer = createDrawerNavigator();
 
@@ -65,20 +51,12 @@ const HomeDrawer = () => {
   return (
     <Drawer.Navigator>
       <Drawer.Screen name="Home" component={HomeTabs} />
-      <Drawer.Screen name="Profile" component={Profile} />
-      <Drawer.Screen
-        name="Password Forget"
-        component={PasswordForgetScreen}
-      />
-      <Drawer.Screen
-        name="Password Change"
-        component={PasswordChangeScreen}
-      />
-
+      <Drawer.Screen name="Account" component={AccountScreen} />
+      <Drawer.Screen name="Password Forget" component={PasswordForgetScreen} />
+      <Drawer.Screen name="Password Change" component={PasswordChangeScreen} />
     </Drawer.Navigator>
   );
 };
-
 
 // APP NAVIGATION
 // -- DRAWER NAV - with left drawer. Home (Trips) / Profile / About / generic links
@@ -87,8 +65,6 @@ const HomeDrawer = () => {
 // -- -- -- Phrases / Currency / Checklist
 
 const RootStack = createStackNavigator();
-
-
 
 const App = () => {
   // const user = auth().currentUser;
@@ -103,77 +79,77 @@ const App = () => {
 
   const handleSignIn = () => {
     // TODO implement real sign in mechanism
- 
+
     setIsAuthenticated(true);
   };
 
   const handleSignOut = () => {
     // TODO implement real sign out mechanism
- 
+
     setIsAuthenticated(false);
   };
 
   const handleSignUp = () => {
     // TODO implement real sign up mechanism
- 
+
     setIsAuthenticated(true);
   };
 
-
   return (
     <NavigationContainer>
-    {/* <RootStack.Navigator initialRouteName="Home">  */}
-    <RootStack.Navigator>
-    {isAuthenticated ? (
-           <RootStack.Screen name="Home" component={HomeDrawer} 
-           options={({ route, navigation }) => ({
-            headerTitle: getFocusedRouteNameFromRoute(route),
-            headerLeft: () => (
-              <Button
-                onPress={() =>
-                  navigation.dispatch(DrawerActions.toggleDrawer())
-                }
-                title="Menu"
-              />
-            ),
-            headerRight: () => (
-               <Button onPress={handleSignOut} title="Sign Out" />
-             ),
+      {/* <RootStack.Navigator initialRouteName="Home">  */}
+      <RootStack.Navigator>
+        {isAuthenticated ? (
+          <RootStack.Screen
+            name="Home"
+            component={HomeDrawer}
+            options={({route, navigation}) => ({
+              headerTitle: getFocusedRouteNameFromRoute(route),
+              headerLeft: () => (
+                <Button
+                  onPress={() =>
+                    navigation.dispatch(DrawerActions.toggleDrawer())
+                  }
+                  title="Menu"
+                />
+              ),
+              headerRight: () => (
+                <Button onPress={handleSignOut} title="Sign Out" />
+              ),
             })}
-           />
-    ) : (
-      <>
-      <RootStack.Screen name="Landing" component={LandingScreen}  options={{
-         title: 'Landing title',
-         headerStyle: {
-          backgroundColor: '#f4511e'
-         },
-         headerTintColor: '#fff',
-         headerTitleStyle: {
-           fontWeight: 'bold',
-         }, animationTypeForReplace: 'pop',
-         }} 
-         />
-      {/* <RootStack.Screen name="Sign In" component={SignInScreen} /> */}
-      <RootStack.Screen name="Sign In">
-         {(props) => (
-           <SignInScreen {...props} onSignIn={handleSignIn} />
-         )}
-      </RootStack.Screen>
-      <RootStack.Screen name="Sign Up">
-         {(props) => (
-           <SignUpScreen {...props} onSignIn={handleSignUp} />
-         )}
-      </RootStack.Screen>
-      <RootStack.Screen 
-        name = "Password Forget"
-        component={PasswordForgetScreen}
-        />
-      </>
-      )}
-    </RootStack.Navigator>
-  </NavigationContainer>
-    
+          />
+        ) : (
+          <>
+            <RootStack.Screen
+              name="Landing"
+              component={LandingScreen}
+              options={{
+                title: 'Landing title',
+                headerStyle: {
+                  backgroundColor: '#f4511e',
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                  fontWeight: 'bold',
+                },
+                animationTypeForReplace: 'pop',
+              }}
+            />
+            {/* <RootStack.Screen name="Sign In" component={SignInScreen} /> */}
+            <RootStack.Screen name="Sign In">
+              {(props) => <SignInScreen {...props} onSignIn={handleSignIn} />}
+            </RootStack.Screen>
+            <RootStack.Screen name="Sign Up">
+              {(props) => <SignUpScreen {...props} onSignIn={handleSignUp} />}
+            </RootStack.Screen>
+            <RootStack.Screen
+              name="Password Forget"
+              component={PasswordForgetScreen}
+            />
+          </>
+        )}
+      </RootStack.Navigator>
+    </NavigationContainer>
   )
 }
 
---
+++ src/screens/Phrases/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+ 
+const PhrasesScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Currency Conversions</Text>
+    </View>
+  );
+};
+ 
+export default PhrasesScreen;
